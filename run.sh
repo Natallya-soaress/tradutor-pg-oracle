067c41147afcb48be64b091fc1b12ed04e7cec18
natallya-soaress <natallyasoares12gmail.com>
2023-07-05 21:39:23 -0300

    correção

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ if flex scanner.l; then
     if yacc -d parser.y; then
         if gcc lex.yy.c y.tab.c -o lexical_analysis -lfl; then
             ./lexical_analysis < input.txt 
-            cat result.sql
         fi
     fi
 fi
